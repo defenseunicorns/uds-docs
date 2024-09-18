f7e5632d81fb8f4a768ee84c15fe5a7a48ba3d42
Paul DiPietro <paul@Yuffie.local>
2024-09-18 15:11:31 -0400

    Add temporary beta banner

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,24 @@
-import { defineCollection } from 'astro:content';
+import {defineCollection, z} from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+// This is a universal banner for beta testing purposes.
+// See: https://github.com/withastro/starlight/discussions/1535
+//
+// Revert to the original code after the bannner is no longer needed!
+
+// export const collections = {
+// 	docs: defineCollection({ schema: docsSchema() }),
+// };
+
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({
+		schema: docsSchema({
+			extend: z.object({
+				banner: z.object({ content: z.string() }).default({
+					content:
+						'This documentation site is in beta testing. Please report any issues <a href="https://github.com/defenseunicorns/uds-docs-refresh" target="_blank">here</a>.',
+				}),
+			}),
+		}),
+	}),
 };
