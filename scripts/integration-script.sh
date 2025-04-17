009a8eb63dd4e945a262b75320cafba303bad536
Micah Nagel <micah.nagel@defenseunicorns.com>
2025-04-17 16:32:04 -0600

    fix: remove ADR folder to prevent build errors

--- scripts/integration-script.sh
+++ scripts/integration-script.sh
@@ -48,5 +48,6 @@ done
 # Clean up and remove the temp folder
 rm -rf temp
 
-# Remove the dev docs folders if present
+# Remove the dev and adr docs folders if present
 rm -rf "$TARGET_DIR/dev"
+rm -rf "$TARGET_DIR/adr"
