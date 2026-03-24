753ba0dc8b164b24a3951024bd2cd46809a13a17
UnicornChance <chance@defenseunicorns.com>
2026-03-24 07:22:16 -0600

    fix: page reloading simplified

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare global {
+  interface Window {
+    /** Bridged from Head.astro for is:inline scripts that can't use ES imports. */
+    __astroNavigate?: (href: string) => void;
+  }
+}
+export {};
